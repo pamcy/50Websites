471b4155dba61aa5ce1583444a3edcf0c343df9a
Chinyi <chinyi3005@gmail.com>
2016-11-20 17:58:03 +0800

    add some comments to explain js scripts for better understanding

--- 11-simple-slider/main.js
+++ 11-simple-slider/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	
 	//設定 wrapper 的長寬，包住所有橫向排列的 slideshow
 	wrapper.css({
-		'position': 'relative',
+//		'position': 'relative',
 		'width'   : slideshow_width,
 		'height'  : slideshow_height
   	});
@@ -26,31 +26,50 @@ $(document).ready(function(){
 	});
 	
 	//點選圓點列會發生以下
-	nav.find('a').on('click', function(pos) {
+	nav.find('a').on('click', function(e) {
+		e.preventDefault();
+		
+		clearInterval(autoSlide); // 停止自動捲動
+		
 		//點圓點變色，找出相對應的 slide 位置
-		nav.find('.active').removeClass('active');
+		nav.find('.active').removeClass('active');		
 		$(this).addClass('active');
-		pos = $(this).index() * slideshow.width();
+		var pos = $(this).index() * slideshow.width();//算出要移動多少距離
 		
 		//每 3 秒輪播自動往左移動的效果
 		//The setInterval() method will continue calling the function until clearInterval() is called.
-		wrapper.animate({left: '-' + pos + 'px'}, 600);
-		clearInterval(autoSlide);
+		wrapper.animate({left: '-' + pos + 'px'}, 600);　// e.g. wrapper.animate({left: '-200px'}, 600);
+		
 		autoSlide = setInterval(slideAuto, 3000);
-    	return false;
 	}).first().addClass('active');
-
+	
+	// 以下兩行　＝　樓上
+	//	nav.find('a').on('click', function(e) { 
+	//		// xxxxxxxxxxxxx
+	//	});
+	//	nav.find('a').first().addClass('active');
+	//^ 網頁一旦 ready，顯示第一張 slideshow 
+	
+	
+	
 	//輪播到最後一張時，再重新跳回第一張顯示？
 	function slideAuto() {
 		if (nav.find('.active').next().length) {
-			nav.find('.active').next().trigger('click');
+			
+			// length == 1, 當被選取的這張還有下一張時
+
+			// nav.find('.active').next().trigger('click');
+			nav.find('.active').next().click();
 		}
 		else {
+			// length == 0 ，沒有下一張時
 			nav.find('a').first().trigger('click');
 		}
 	}
+	
+	
 	autoSlide = setInterval(slideAuto, 3000);
-});
+});//^ ready
 
 //QUESTION
 //1. 為什麼 i + 1
