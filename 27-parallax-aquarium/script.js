aac0b0588dbbc6a00663c64fd721f33851686dec
Pamcy <chinyi3005@gmail.com>
2017-01-09 19:08:02 +0800

    Update: store all window.pageYOffset in a variables.

--- 27-parallax-aquarium/script.js
+++ 27-parallax-aquarium/script.js
@@ -6,17 +6,20 @@ var f1 = document.getElementById('fish-01'),
 	f6 = document.getElementById('fish-06');
 
 function swimming() {
-	f1.style.left = ((window.pageYOffset / 10) - 400) + 'px';
-	f2.style.left = ((window.pageYOffset / 5) + 250) + 'px';
 	
-	f3.style.left = ((window.pageYOffset / 6) + 150) + 'px';
-	f3.style.bottom = ((window.pageYOffset / 35) + 50) + 'px';
+	var windowYOffset = window.pageYOffset;
 	
-	f4.style.right = ((window.pageYOffset / 6) - 200) + 'px';
-	f4.style.bottom = (window.pageYOffset / 35) + 'px';
+	f1.style.left = ((windowYOffset / 10) - 400) + 'px';
+	f2.style.left = ((windowYOffset / 5) + 250) + 'px';
 	
-	f5.style.right = ((window.pageYOffset / 6) + 380) + 'px';
-	f6.style.right = ((window.pageYOffset / 15) - 50) + 'px';
+	f3.style.left = ((windowYOffset / 6) + 150) + 'px';
+	f3.style.bottom = ((windowYOffset / 35) + 50) + 'px';
+	
+	f4.style.right = ((windowYOffset / 6) - 200) + 'px';
+	f4.style.bottom = (windowYOffset / 35) + 'px';
+	
+	f5.style.right = ((windowYOffset / 6) + 380) + 'px';
+	f6.style.right = ((windowYOffset / 15) - 50) + 'px';
 }
 
 window.addEventListener('scroll', swimming);
