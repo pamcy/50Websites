3c7ff1cdc976a1d8b4f71e3cef37b8bb15bebba1
Pamcy <chinyi3005@gmail.com>
2017-01-15 16:08:42 +0800

    Fixed: When menu is clicked, stop running the slideshow automatically.

--- 29-fade-slideshow/script.js
+++ 29-fade-slideshow/script.js
@@ -2,14 +2,15 @@ $(document).ready(function () {
 	var nav = $('.slideshow-nav li'),
 		slides = $('.slideshow-slides li'),
 		slidesAll = slides.length,
-		nextSlide = 0;
+		nextSlide = 0,
+		timer;
 
 	// Play Slideshow and nav menu automatically
 	function playSideshow() {
 		slides.eq(nextSlide).addClass('slideshow-slides-isActive').siblings().removeClass('slideshow-slides-isActive');
 		nav.eq(nextSlide).addClass('slideshow-nav-isActive').siblings().removeClass('slideshow-nav-isActive');
 		nextSlide = (nextSlide + 1) % slidesAll;
-		setTimeout(playSideshow, 4000);
+		timer = setTimeout(playSideshow, 4000);
 	}
 
 	playSideshow();
@@ -19,9 +20,9 @@ $(document).ready(function () {
 		e.preventDefault();
 
 		var navIndex = $(this).index();
-		
+
 		nav.eq(navIndex).addClass('slideshow-nav-isActive').siblings().removeClass('slideshow-nav-isActive');
 		slides.eq(navIndex).addClass('slideshow-slides-isActive').siblings().removeClass('slideshow-slides-isActive');
-		nextSlide = (navIndex + 1) % slidesAll;
+		clearInterval(timer);
 	});
 });
