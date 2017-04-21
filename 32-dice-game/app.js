f47d96e1a2506eb2562b96ec60ba6648ad08a687
Chinyi Pam <chinyi.pam@gmail.com>
2017-04-21 11:23:39 +0800

    Add rule: When two 6 in a row

--- 32-dice-game/app.js
+++ 32-dice-game/app.js
@@ -9,68 +9,59 @@ GAME RULES:
 
 */
 
-var totalScore, currentScore, activePlayer, gamePlaying;
+let totalScore;
+let currentScore;
+let activePlayer;
+let gamePlaying;
+let lastDiceNumber;
 
-function init() {
+function changePlayer() {
     currentScore = 0;
-    totalScore = [0, 0];
-    activePlayer = 0;
-    gamePlaying = true;
-
-    document.querySelector('.dice').style.display = 'none';
-    
-    document.querySelector('.btn-roll').style.display = 'block';
-    document.querySelector('.btn-hold').style.display = 'block';
-    document.getElementById('current-box-0').style.display = 'block';
-    document.getElementById('current-box-1').style.display = 'block';
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
     document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0'; 
+    document.getElementById('current-1').textContent = '0';
 
-    document.getElementById('score-0').textContent = '0';
-    document.getElementById('score-1').textContent = '0';
-
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
-
-    document.querySelector('.player-0-panel .player-winner').style.display = 'none';
-    document.querySelector('.player-1-panel .player-winner').style.display = 'none';
-
-    document.querySelector('.player-0-panel').classList.remove('winner');
-    document.querySelector('.player-1-panel').classList.remove('winner');
-    
-    document.querySelector('.player-1-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.add('active');
-
-    document.querySelector('.btn-roll').addEventListener('click', rollingDice);
-    document.querySelector('.btn-hold').addEventListener('click', holding);
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
 }
 
 function rollingDice() {
-    var diceNumber = Math.floor(Math.random() * 6) + 1, // Shuffle numbers
-        diceDOM = document.querySelector('.dice');
+    const currentDiceNumber = Math.floor(Math.random() * 6) + 1; // Shuffle numbers
+    const diceDOM = document.querySelector('.dice');
 
     if (gamePlaying) {
         // Display dice images
         diceDOM.style.display = 'block';
-        diceDOM.src = `imgs/dice-${diceNumber}.svg`;
- 
+        diceDOM.src = `imgs/dice-${currentDiceNumber}.svg`;
+        console.log(`current one: ${currentDiceNumber}`);
+
         diceDOM.classList.add('animated', 'bounceIn');
-        setTimeout(function() {
+        setTimeout(function () {
             diceDOM.classList.remove('animated', 'bounceIn');
         }, 500);
 
         // Update current scores
-        if (diceNumber === 1) {
-            setTimeout(function() {
+        if (currentDiceNumber === 1) {
+            setTimeout(function () {
+                document.querySelector('.dice').style.display = 'none';
+                setTimeout(changePlayer, 100);
+            }, 500);
+        } else if (lastDiceNumber === 6 && currentDiceNumber === 6) {
+            totalScore[activePlayer] = 0;
+            document.getElementById(`score-${activePlayer}`).textContent = '0';
+
+            setTimeout(function () {
                 document.querySelector('.dice').style.display = 'none';
                 setTimeout(changePlayer, 100);
             }, 500);
         } else {
-            currentScore += diceNumber;
+            currentScore += currentDiceNumber;
             document.getElementById(`current-${activePlayer}`).textContent = currentScore;
         }
+
+        console.log(`Last one: ${lastDiceNumber}`);
+        lastDiceNumber = currentDiceNumber;
     }
 }
 
@@ -93,7 +84,7 @@ function holding() {
 
             // Display winner image
             document.querySelector(`.player-${activePlayer}-panel .player-winner`).style.display = 'block';
-            
+
             gamePlaying = false;
         } else {
             changePlayer();
@@ -101,15 +92,39 @@ function holding() {
     }
 }
 
-function changePlayer() {
+function init() {
     currentScore = 0;
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    totalScore = [0, 0];
+    activePlayer = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.btn-roll').style.display = 'block';
+    document.querySelector('.btn-hold').style.display = 'block';
+    document.getElementById('current-box-0').style.display = 'block';
+    document.getElementById('current-box-1').style.display = 'block';
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel .player-winner').style.display = 'none';
+    document.querySelector('.player-1-panel .player-winner').style.display = 'none';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+    document.querySelector('.btn-roll').addEventListener('click', rollingDice);
+    document.querySelector('.btn-hold').addEventListener('click', holding);
 }
 
 function openInfo() {
@@ -119,15 +134,17 @@ function openInfo() {
         html:
             `<b>Be the first player to reach 100 points</b><br><br>
             <p style="text-align: left">
-                Rolls a 1:<br>
-                Scores nothing and change player.<br><br>
+                Roll a 1:<br>
+                Score zero and next player turn.<br><br>
+                Roll two 6:<br>
+                Lose ENTIRE score and next player turn.<br><br>
                 Hold:<br>
-                Stop rolling, record the total and change player.
+                Stop rolling, record the total and next player turn.
             </p>`,
         padding: 60,
         allowEnterKey: true,
         confirmButtonText:
-            'OK'
+            'OK',
     })
 }
 
