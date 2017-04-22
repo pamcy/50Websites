17b9d2f78629be8da3d6d4ae769113a3943d0bc5
Chinyi Pam <chinyi.pam@gmail.com>
2017-04-22 19:57:21 +0800

    Change to 2 dices in the game

--- 32-dice-game/challenge.js
+++ 32-dice-game/challenge.js
@@ -2,6 +2,7 @@ let totalScore;
 let currentScore;
 let activePlayer;
 let gamePlaying;
+let lastDiceNumber;
 
 function changePlayer() {
     currentScore = 0;
@@ -14,37 +15,42 @@ function changePlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
 }
 
-function shuffle() {
-    return Math.floor(Math.random() * 6) + 1;
-}
-
 function rollingDice() {
-    const diceDOM = document.querySelectorAll('.dice');
+    const currentDiceNumber = Math.floor(Math.random() * 6) + 1; // Shuffle numbers
+    const diceDOM = document.querySelector('.dice');
 
     if (gamePlaying) {
-        for (let i = 0; i < diceDOM.length; i++) {
-            const currentDiceNumber = shuffle();
+        // Display dice images
+        diceDOM.style.display = 'block';
+        diceDOM.src = `imgs/dice-${currentDiceNumber}.svg`;
+        console.log(`current one: ${currentDiceNumber}`);
 
-            // Display dice images
-            diceDOM[i].style.display = 'block';
-            diceDOM[i].src = `imgs/dice-${currentDiceNumber}.svg`;
-            diceDOM[i].classList.add('animated', 'bounceIn');
+        diceDOM.classList.add('animated', 'bounceIn');
+        setTimeout(function () {
+            diceDOM.classList.remove('animated', 'bounceIn');
+        }, 500);
+
+        // Update current scores
+        if (currentDiceNumber === 1) {
             setTimeout(function () {
-                diceDOM[i].classList.remove('animated', 'bounceIn');
+                diceDOM.style.display = 'none';
+                setTimeout(changePlayer, 100);
             }, 500);
+        } else if (lastDiceNumber === 6 && currentDiceNumber === 6) {
+            totalScore[activePlayer] = 0;
+            document.getElementById(`score-${activePlayer}`).textContent = '0';
 
-            // Update current scores
-            if (currentDiceNumber === 1) {
-                setTimeout(function () {
-                    diceDOM[0].style.display = 'none';
-                    diceDOM[1].style.display = 'none';
-                    setTimeout(changePlayer, 100);
-                }, 500);
-            } else {
-                currentScore += currentDiceNumber;
-                document.getElementById(`current-${activePlayer}`).textContent = currentScore;
-            }
+            setTimeout(function () {
+                diceDOM.style.display = 'none';
+                setTimeout(changePlayer, 100);
+            }, 500);
+        } else {
+            currentScore += currentDiceNumber;
+            document.getElementById(`current-${activePlayer}`).textContent = currentScore;
         }
+
+        console.log(`Last one: ${lastDiceNumber}`);
+        lastDiceNumber = currentDiceNumber;
     }
 }
 
@@ -67,8 +73,7 @@ function holding() {
             document.getElementById(`name-${activePlayer}`).textContent = 'Winner!';
             document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
 
-            document.querySelectorAll('.dice')[0].style.display = 'none';
-            document.querySelectorAll('.dice')[1].style.display = 'none';
+            document.querySelector('.dice').style.display = 'none';
             document.querySelector('.btn-roll').style.display = 'none';
             document.querySelector('.btn-hold').style.display = 'none';
             document.getElementById(`current-box-${activePlayer}`).style.display = 'none';
@@ -89,21 +94,23 @@ function init() {
     activePlayer = 0;
     gamePlaying = true;
 
-    document.querySelector('.dice-0').style.display = 'none';
-    document.querySelector('.dice-1').style.display = 'none';
+    document.querySelector('.dice').style.display = 'none';
     document.querySelector('.btn-roll').style.display = 'block';
     document.querySelector('.btn-hold').style.display = 'block';
     document.getElementById('current-box-0').style.display = 'block';
     document.getElementById('current-box-1').style.display = 'block';
-    document.querySelector('.player-0-panel .player-winner').style.display = 'none';
-    document.querySelector('.player-1-panel .player-winner').style.display = 'none';
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
+
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
+
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel .player-winner').style.display = 'none';
+    document.querySelector('.player-1-panel .player-winner').style.display = 'none';
 
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
@@ -125,6 +132,8 @@ function openInfo() {
             <p style="text-align: left">
                 Roll a 1:<br>
                 Score zero and next player turn.<br><br>
+                Roll two 6 in a row:<br>
+                Lose ENTIRE score and next player turn.<br><br>
                 Hold:<br>
                 Stop rolling, record the total and next player turn.
             </p>`,
