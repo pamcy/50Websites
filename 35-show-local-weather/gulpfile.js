dae7eea9d9fee2196dcc53927cbd56030917f5b0
Chinyi Pam <chinyi.pam@gmail.com>
2017-06-13 16:26:00 +0800

    Change folder structrue

---
+++ 35-show-local-weather/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function () {
+    gulp.src('scss/style.scss')
+        .pipe(sass({ includePaths: ['scss'] }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(['*.html', 'css/*.css', 'js/*.js'], {
+        server: {
+            baseDir: './',
+        },
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch('scss/*.scss', ['sass']);
+});
