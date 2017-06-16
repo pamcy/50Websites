4cf97e4241d954e961caa544886af827567b1ebc
Chinyi Pam <chinyi.pam@gmail.com>
2017-06-16 16:40:58 +0800

    Change gulp to arrow functions

--- 35-show-local-weather/gulpfile.js
+++ 35-show-local-weather/gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
     gulp.src('scss/style.scss')
         .pipe(sass({ includePaths: ['scss'] }))
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', () => {
     browserSync.init(['*.html', 'css/*.css', 'js/*.js'], {
         server: {
             baseDir: './',
@@ -16,6 +16,6 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function () {
+gulp.task('default', ['sass', 'browser-sync'], () => {
     gulp.watch('scss/*.scss', ['sass']);
 });
