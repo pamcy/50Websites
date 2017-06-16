f8ebfbbb9db07e0a9329190c6d55671f0c04c8b5
Chinyi Pam <chinyi.pam@gmail.com>
2017-06-16 16:37:44 +0800

    Fix gulp eslint problems

--- 35-show-local-weather/gulpfile.js
+++ 35-show-local-weather/gulpfile.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
     gulp.src('scss/style.scss')
