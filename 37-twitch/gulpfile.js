10002abe478bcb3f3c4c122c5914b1e445da9149
Pamcy <chinyi.pam@gmail.com>
2017-09-12 15:13:54 +0800

    [no.37] Add Postcss

--- 37-twitch/gulpfile.js
+++ 37-twitch/gulpfile.js
@@ -1,18 +1,23 @@
 const gulp         = require('gulp')
 const sass         = require('gulp-sass')
 const sourcemaps   = require('gulp-sourcemaps')
-const autoprefixer = require('gulp-autoprefixer')
+const postcss      = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano      = require('cssnano')
 const uglify       = require('gulp-uglify')
 const babel        = require('gulp-babel')
 const browserSync  = require('browser-sync').create()
 
 gulp.task('make:css', () => {
+    const plugins = [
+        autoprefixer({ browsers: ['last 2 versions'] }),
+        cssnano(),
+    ]
+
     return gulp.src('src/scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-        }))
+        .pipe(postcss(plugins))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('public/css'))
         .pipe(browserSync.stream())
@@ -27,7 +32,7 @@ gulp.task('make:js', () => {
         .pipe(browserSync.stream())
 })
 
-gulp.task('make:html', () => {
+gulp.task('copy:html', () => {
     return gulp.src('src/**/*.html')
         .pipe(gulp.dest('public'))
         .pipe(browserSync.stream())
@@ -45,7 +50,7 @@ gulp.task('browserSync', () => {
 // Array of tasks to complete before watch
 gulp.task('default', ['browserSync', 'make:css', 'make:js'], () => {
     gulp.watch('src/scss/**/*.scss', ['make:css'])
-    gulp.watch('src/*.html', ['make:html'])
     gulp.watch('src/js/**/*.js', ['make:js'])
+    gulp.watch('src/*.html', ['copy:html'])
 })
 
