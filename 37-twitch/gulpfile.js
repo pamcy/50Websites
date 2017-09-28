80e2656be46f15897d1b4576368a89ee35645c9d
Pamcy <chinyi.pam@gmail.com>
2017-09-28 08:57:16 +0800

    [no.37] :hammer: Refactor gulpfile

--- 37-twitch/gulpfile.js
+++ 37-twitch/gulpfile.js
@@ -1,56 +1,70 @@
-const gulp         = require('gulp')
-const sass         = require('gulp-sass')
-const sourcemaps   = require('gulp-sourcemaps')
-const postcss      = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
-const cssnano      = require('cssnano')
-const uglify       = require('gulp-uglify')
-const babel        = require('gulp-babel')
-const browserSync  = require('browser-sync').create()
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const sourcemaps   = require('gulp-sourcemaps');
+const postcss      = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano      = require('cssnano');
+const browserSync  = require('browser-sync').create();
+// const uglify    = require('gulp-uglify')
+// const babel     = require('gulp-babel')
+
+
+/*
+ * sub tasks
+ */
 
 gulp.task('make:css', () => {
-    const plugins = [
-        autoprefixer({ browsers: ['last 2 versions'] }),
-        cssnano(),
-    ]
-
     return gulp.src('src/scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(plugins))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('public/css'))
-        .pipe(browserSync.stream())
-})
+        .pipe(browserSync.stream());
+});
 
-gulp.task('make:js', () => {
-    return gulp.src('src/js/**/*.js')
-        // plugin "babel-preset-env"
-        .pipe(babel({ presets: ['env'] }))
-        .pipe(uglify())
-        .pipe(gulp.dest('public/js'))
-        .pipe(browserSync.stream())
-})
+gulp.task('process:css', ['make:css'], () => {
+    const plugins = [
+        autoprefixer({ browsers: ['last 2 versions'] }),
+        cssnano(),
+    ];
+
+    return gulp.src('public/css/**/*.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('public/css'));
+});
+
+// gulp.task('make:js', () => {
+//     return gulp.src('src/js/**/*.js')
+//         // plugin "babel-preset-env"
+//         .pipe(babel({ presets: ['env'] }))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('public/js'))
+//         .pipe(browserSync.stream())
+// })
 
 gulp.task('copy:html', () => {
     return gulp.src('src/**/*.html')
         .pipe(gulp.dest('public'))
-        .pipe(browserSync.stream())
-})
+        .pipe(browserSync.stream());
+});
 
-// Run BrowserSync
 gulp.task('browserSync', () => {
     browserSync.init({
         server: {
             baseDir: 'public',
         },
-    })
-})
+    });
+});
+
+
+/*
+ * main tasks
+ */
+
+gulp.task('build', ['copy:html', 'process:css']);
 
 // Array of tasks to complete before watch
-gulp.task('default', ['browserSync', 'make:css', 'make:js'], () => {
-    gulp.watch('src/scss/**/*.scss', ['make:css'])
-    gulp.watch('src/js/**/*.js', ['make:js'])
-    gulp.watch('src/*.html', ['copy:html'])
-})
-
+gulp.task('default', ['browserSync', 'make:css', 'copy:html'], () => {
+    gulp.watch('src/*.html', ['copy:html']);
+    gulp.watch('src/scss/**/*.scss', ['make:css']);
+});
