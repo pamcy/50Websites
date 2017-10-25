0ffd6e85f9477250ba71eeac41c48f77d52657b2
Pamcy <chinyi.pam@gmail.com>
2017-10-25 10:45:31 +0800

    [no.38] :sparkles: Build `hero-carousel`

--- 38-madegentle-website/src/js/carousel.js
+++ 38-madegentle-website/src/js/carousel.js
@@ -1,9 +1,17 @@
-$(function () {
+$(document).ready(() => {
     $('#js-single-slick').slick({
         slidesToShow: 1,
         arrows: false,
         infinite: true,
         speed: 500,
         autoplay: true,
+    });
+
+    $('#js-hero-slick').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        appendArrows: '.hero-carousel__arrow-btns',
+        prevArrow: '<button class="hero-carousel__arrow-btn hero-carousel__left-arrow-btn"><i class="icon-navigate_before hero-carousel__arrow-icon"></i></button>',
+        nextArrow: '<button class="hero-carousel__arrow-btn hero-carousel__right-arrow-btn"><i class="icon-navigate_next hero-carousel__arrow-icon"></i></button>',
     });
 });
