b4ad08d328ac5af3295ff1f69d89849c0556ad79
Pamcy <chinyi.pam@gmail.com>
2017-11-28 17:57:56 +0800

    [no.39] :sparkles: Animate the farthest distance

--- 39-admin-space-control/dist/js/main.js
+++ 39-admin-space-control/dist/js/main.js
@@ -1,9 +1,30 @@
 'use strict';
 
-function toggleMenu() {
-    $(this).parent().toggleClass('nav-is-open');
-}
+var number_field = $('.farthest__number');
+var max_number = number_field.data('max-number');
+var current_number = 0;
+
+// Menu navigation
+var toggleMenu = function toggleMenu() {
+    $(undefined).parent().toggleClass('nav-is-open');
+};
+
+// Animate the farthest distance
+var showNumber = function showNumber() {
+    current_number += 195128740;
+    number_field.html(current_number.toLocaleString() + ' km');
+    animateNumber();
+};
+
+var animateNumber = function animateNumber() {
+    if (current_number < max_number) {
+        requestAnimationFrame(showNumber);
+    } else {
+        number_field.html(max_number.toLocaleString() + ' km');
+    }
+};
 
 $(document).ready(function () {
     $('.nav__btn').on('click', toggleMenu);
+    showNumber();
 });
--- 39-admin-space-control/src/js/main.js
+++ 39-admin-space-control/src/js/main.js
@@ -1,7 +1,28 @@
-function toggleMenu() {
+const number_field = $('.farthest__number');
+const max_number = number_field.data('max-number');
+let current_number = 0;
+
+// Menu navigation
+const toggleMenu = () => {
     $(this).parent().toggleClass('nav-is-open');
-}
+};
+
+// Animate the farthest distance
+const showNumber = () => {
+    current_number += 195128740;
+    number_field.html(`${current_number.toLocaleString()} km`);
+    animateNumber();
+};
+
+const animateNumber = () => {
+    if (current_number < max_number) {
+        requestAnimationFrame(showNumber);
+    } else {
+        number_field.html(`${max_number.toLocaleString()} km`);
+    }
+};
 
 $(document).ready(() => {
     $('.nav__btn').on('click', toggleMenu);
+    showNumber();
 });
