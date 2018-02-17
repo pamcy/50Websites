1a322857ddbbf38be56ea511d7a1370e7997587c
Pamcy <chinyi.pam@gmail.com>
2018-02-17 14:58:52 +0800

    [no.38] :art: Update JS structure

--- 38-madegentle-website/src/js/carousel.js
+++ 38-madegentle-website/src/js/carousel.js
@@ -1,4 +1,7 @@
 $(document).ready(() => {
+    // WOW Plugin
+    new WOW().init();
+
     $('#js-single-slick').slick({
         slidesToShow: 1,
         arrows: false,
@@ -9,7 +12,7 @@ $(document).ready(() => {
 
     $('#js-hero-slick').slick({
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2500,
         appendArrows: '.hero-carousel__arrow-btns',
         prevArrow: '<button class="hero-carousel__arrow-btn hero-carousel__left-arrow-btn"><i class="icon-navigate_before hero-carousel__arrow-icon"></i></button>',
         nextArrow: '<button class="hero-carousel__arrow-btn hero-carousel__right-arrow-btn"><i class="icon-navigate_next hero-carousel__arrow-icon"></i></button>',
--- 38-madegentle-website/src/js/typing.js
+++ 38-madegentle-website/src/js/typing.js
@@ -1,6 +1,6 @@
 $(function () {
     $('#js-typeit').typeIt({
-        strings: ['Modern Combination', 'Creative Solutions'],
+        strings: ['Modern Combination', 'Focus on craftsmanship'],
         speed: 100,
         breakDelay: 750,
         breakLines: false,
