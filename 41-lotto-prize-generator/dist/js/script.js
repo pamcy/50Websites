ecd233a99e718f786ff2c80c43c00898ba51d457
Pamcy <chinyi.pam@gmail.com>
2018-04-26 14:15:51 +0800

    :sparkles: Add no.41 project: Lotto prize generator

---
+++ 41-lotto-prize-generator/dist/js/script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var prizes = {
+    0: '🙈',
+    1: '🤢',
+    2: '💩',
+    3: '🦊',
+    4: '👀',
+    5: '👾',
+    6: '👻',
+    7: '🤖'
+};
+var total_items = 8;
+var minimum_jumps = 30; // 超過這數字開始進入抽獎
+var current_index = -1;
+var jumps = 0; // 每次跑一格就是一個 jump
+var speed = 30;
+var timer = 0;
+var prize = -1;
+
+function runCircle() {
+    $('[data-order="' + current_index + '"]').removeClass('is-active');
+
+    // 順序很重要
+    current_index += 1;
+
+    if (current_index > total_items - 1) {
+        current_index = 0;
+    }
+
+    $('[data-order="' + current_index + '"]').addClass('is-active');
+}
+
+function generatePrizeNumber() {
+    return Math.floor(Math.random() * total_items);
+}
+
+function controllSpeed() {
+    jumps += 1;
+    runCircle();
+
+    // 1. 抽到獎品停止遊戲
+    if (jumps > minimum_jumps + 10 && prize === current_index) {
+        clearTimeout(timer);
+
+        swal({
+            title: 'You Have Won a Prize ' + prizes[current_index],
+            text: 'Congratulations!',
+            icon: 'success'
+        });
+
+        prize = -1;
+        jumps = 0;
+
+        // 2. 還沒抽繼續跑
+    } else {
+        // 還沒進入關鍵抽獎階段前的速度 (單純前菜轉特效)
+        if (jumps < minimum_jumps) {
+            speed -= 5; // 加速
+            // 決定獎品的位置
+        } else if (jumps === minimum_jumps) {
+            var random_number = generatePrizeNumber();
+            prize = random_number;
+        } else {
+            // 下一個就是獎品時放慢鈍一下
+            if (jumps > minimum_jumps + 10 && prize === current_index + 1) {
+                speed += 600;
+            } else {
+                speed += 20; // 減慢
+            }
+        }
+        if (speed < 40) {
+            speed = 40;
+        }
+        // 重複跑這支 function
+        timer = setTimeout(controllSpeed, speed);
+    }
+
+    // console.log({current_index, jumps});
+}
+
+function init() {
+    jumps = 0;
+    speed = 100;
+    prize = -1;
+
+    controllSpeed();
+}
+
+$(document).ready(function () {
+    $('#js-start').on('click', init);
+});
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 I knew that if I was going to succeed at learning to code, I would need to apply knowledge as much as possible. I devoted to build something from what I learned, below is a list of websites I made lately, some took a couple of days to complete. (Still Updating)
 
+- [x] [41 - Lotto Prize Generator](https://pamcy.github.io/50Websites/41-lotto-prize-generator/)
+
 - [x] [40 - HTML5 Canvas Drawing Board](https://pamcy.github.io/50Websites/40-canvas-drawing-board/)
 
 - [x] [39 - Responsive Animated Space Control](https://pamcy.github.io/50Websites/39-admin-space-control/)
