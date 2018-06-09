f919111eb9b9292b78c8a60552c16b37adf90999
pamcy <chinyi.pam@gmail.com>
2018-06-09 09:12:50 +0800

    [no.45] Fetch and display modal content

--- 45-movie-app/js/main.js
+++ 45-movie-app/js/main.js
@@ -1,20 +1,21 @@
-// const menu_items = document.querySelectorAll('.sidebar__item');
-const list_container = document.querySelector('.main__movie-list');
 const top_playing_btn = document.querySelector('#js-top-playing');
 const upcoming_btn = document.querySelector('#js-upcoming');
 const top_rated_btn = document.querySelector('#js-top-rated');
 const heading = document.querySelector('.main__heading');
+const list_container = document.querySelector('.main__movie-list');
+const modal = document.querySelector('.overlay');
+const modal_close_btn = document.querySelector('.overlay__close-icon');
 const genres_storage = {};
 const now_playing_storage = [];
 const upcoming_storage = [];
 const top_rated_storage = [];
-let credits_storage = [];
-let videos_storage = [];
-let reviews_storage = [];
+let details_storage;
+let casts_storage;
+let videos_storage;
 
 const api = {
   key: 'ffea606c542fc11ca9059abde95f3e90',
-  img_prefix: 'https://image.tmdb.org/t/p/w500',
+  img_prefix: 'https://image.tmdb.org/t/p/w',
 
   getGenreUrl() {
     return `https://api.themoviedb.org/3/genre/movie/list?api_key=${this.key}&language=en-US`;
@@ -28,16 +29,16 @@ const api = {
   getTopRatedUrl() {
     return `https://api.themoviedb.org/3/movie/top_rated?api_key=${this.key}&language=en-US&page=1`;
   },
+  getMovieDetailsUrl(id) {
+    return `https://api.themoviedb.org/3/movie/${id}?api_key=${this.key}`;
+  },
+  getCastsUrl(id) {
+    return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${this.key}`;
+  },
+  getVideoUrl(id) {
+    return `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${this.key}`;
+  },
 };
-
-// const key = 'ffea606c542fc11ca9059abde95f3e90';
-// const now_playing_url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`;
-// const upcoming_url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`;
-// const movie_details_url = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&language=en-US`;
-// const movie_credits_url = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${key}`;
-// const movie_video_url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${key}&language=en-US`;
-// const movie_review_url = `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${key}&language=en-US&page=1`;
-
 
 const movie = {
   fetchGenre(url) {
@@ -46,7 +47,6 @@ const movie = {
       .then((data) => {
         for (const item of data.genres) {
           genres_storage[item.id] = item.name;
-          // console.log(genres_storage);
         }
       });
   },
@@ -63,17 +63,15 @@ const movie = {
     const list_content = storage
       .map((item) => {
         return `
-        <li class="main__movie-item">
-          <a href="#" class="main__movie-item-link">
-            <div class="main__movie-top">
-              <img src="${api.img_prefix}${item.poster_path}" alt="${item.original_title}" class="main__movie-poster">
-              <span class="main__movie-votes">${item.vote_average}</span>
-            </div>
-            <div class="main__movie-bottom">
-              <h3 class="main__movie-name">${item.original_title}</h3>
-              <h4 class="main__movie-category">${item.genre_ids.map(id => movie.displayGenres(id)).join(', ')}</h4>
-            </div>
-          </a>
+        <li class="main__movie-item" data-id="${item.id}">
+          <div class="main__movie-top">
+            <img src="${api.img_prefix}500${item.poster_path}" alt="${item.original_title}" class="main__movie-poster">
+            <span class="main__movie-votes">${item.vote_average}</span>
+          </div>
+          <div class="main__movie-bottom">
+            <h3 class="main__movie-name">${item.original_title}</h3>
+            <h4 class="main__movie-category">${item.genre_ids.map(id => movie.displayGenres(id)).join(', ')}</h4>
+          </div>
         </li>
         `;
       })
@@ -82,135 +80,99 @@ const movie = {
     list_container.innerHTML = list_content;
     heading.textContent = title || 'Now Playing';
   },
-};
+  fetchMovieDetails(id) {
+    return fetch(api.getMovieDetailsUrl(id))
+      .then(response => response.json())
+      .then((data) => {
+        details_storage = data;
+      });
+  },
+  fetchCastsData(id) {
+    return fetch(api.getCastsUrl(id))
+      .then(response => response.json())
+      .then((data) => {
+        casts_storage = data.cast;
+      });
+  },
+  fetchVideosData(id) {
+    return fetch(api.getVideoUrl(id))
+      .then(response => response.json())
+      .then((data) => {
+        videos_storage = data.results;
+      });
+  },
+  writeModalContent(e) {
+    const current_movie = e.target.closest('.main__movie-item');
 
-
-function fetchCredits() {
-  const movie_id = this.dataset.id;
-  const movie_credits_url = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${key}`;
-
-  fetch(movie_credits_url)
-    .then(response => response.json())
-    .then(data => {
-      for (const item of credits_storage) {
-        if (item.id == data.id) {
-          return;
-        }
-
-        credits_storage.push(data);
-        // console.log(`item_id: ${item.id}, data_id: ${data.id}`);
-      }
-    });
-}
-
-function displayCredits() {
-  // console.log(this);
-  const movie_id = this.dataset.id;
-
-  credits_storage.forEach(item => {
-    if (item.id != movie_id) {
+    if (!current_movie) {
       return;
     }
 
-    const display_numbers = 10;
-    const casts = item.cast.slice(0, display_numbers);
-    const casts_content = casts
-      .map(cast => {
+    const movie_id = current_movie.dataset.id;
+    const details = movie.fetchMovieDetails(movie_id);
+    const casts = movie.fetchCastsData(movie_id);
+    const videos = movie.fetchVideosData(movie_id);
+
+    const backdrop = document.querySelector('.overlay__backdrop');
+    const vote = document.querySelector('.overlay__votes');
+    const poster = document.querySelector('.overlay__poster');
+    const name = document.querySelector('.overlay__info-heading');
+    const runtime = document.querySelector('.overlay__info-runtime');
+    const category = document.querySelector('.overlay__info-category');
+    const overview = document.querySelector('.overlay__info-overview');
+    const casts_container = document.querySelector('.overlay__cast-list');
+    const video_container = document.querySelector('.overlay__video-wrapper');
+
+    details.then(() => {
+      backdrop.style.backgroundImage = `url('${api.img_prefix}1280/${details_storage.backdrop_path}')`;
+
+      poster.src = `${api.img_prefix}300/${details_storage.poster_path}`;
+
+      vote.textContent = `${details_storage.vote_average}`;
+
+      name.textContent = details_storage.original_title;
+
+      runtime.textContent = `${details_storage.runtime}mins`;
+
+      category.textContent = current_movie.querySelector('.main__movie-category').textContent;
+
+      overview.textContent = `${details_storage.overview}`;
+    });
+
+    casts.then(() => {
+      const casts_limit = 6;
+      const all_casts = casts_storage.splice(0, casts_limit);
+
+      const casts_content = all_casts.map((cast) => {
         return `
-                <img src="${img_prefix}/w200/${
-          cast.profile_path
-        }" style="width: 50px">
-                <p>${cast.name}</p>
-            `;
-      })
-      .join("");
+          <li class="overlay__cast-item">
+            <div class="overlay__cast-img-container">
+              <img src="${api.img_prefix}200${cast.profile_path}" alt="${cast.name}'s photo" class="overlay__cast-img">
+            </div>
+            <span class="overlay__cast-name">${cast.name}</span>
+          </li>
+        `;
+      }).join('');
 
-    // console.log(casts);
-
-    document.querySelector(".modal__content").innerHTML = casts_content;
-  });
-}
-
-function fetchVideos() {
-  const movie_id = this.dataset.id;
-  const movie_video_url = `https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=${key}&language=en-US`;
-
-  fetch(movie_video_url)
-    .then(response => response.json())
-    .then(data => {
-      for (const item of videos_storage) {
-        if (item.id == data.id) {
-          return;
-        }
-      }
-      videos_storage.push(data);
+      casts_container.innerHTML = casts_content;
     });
-}
 
-function displayVideos() {
-  const movie_id = this.dataset.id;
+    videos.then(() => {
+      const youtube_key = videos_storage[0].key;
+      const video_content = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${youtube_key}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
 
-  videos_storage.forEach(item => {
-    if (item.id != movie_id) {
-      return;
-    }
-
-    const youtube_id = item.results[0].key;
-    const youtube_video = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${youtube_id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
-
-    document.querySelector(".movie-info__videos").innerHTML = youtube_video;
-
-    // console.log(videos_storage);
-  });
-}
-
-function fetchReviews() {
-  const movie_id = this.dataset.id;
-  const movie_review_url = `https://api.themoviedb.org/3/movie/${movie_id}/reviews?api_key=${key}&language=en-US&page=1`;
-
-  fetch(movie_review_url)
-    .then(response => response.json())
-    .then(data => {
-      for (const item of reviews_storage) {
-        if (item.id == data.id) {
-          return;
-        }
-      }
-      reviews_storage.push(data);
+      video_container.innerHTML = video_content;
     });
-}
-
-function displayReviews() {
-  const movie_id = this.dataset.id;
-
-  reviews_storage.forEach(item => {
-    if (item.id != movie_id) {
-      return;
+  },
+  openModal(e) {
+    if (e.target.closest('.main__movie-item')) {
+      modal.classList.add('is-open');
     }
-
-    const display_numbers = 5;
-    const reviews = item.results.slice(0, display_numbers);
-    let reviews_content;
-
-    if (reviews.length <= 0) {
-      // console.log('no comment');
-      reviews_content = `No comments yet.`;
-    } else {
-      reviews_content = reviews
-        .map(review => {
-          return `
-                    <a href="${review.url}"><h3>${review.author}</h3></a>
-                    <p>${review.content}</p>
-                `;
-        })
-        .join("");
-    }
-
-    document.querySelector(".modal__reviews").innerHTML = reviews_content;
-
-    // console.log(reviews_storage);
-  });
-}
+  },
+  closeModal() {
+    modal.classList.remove('is-open');
+  },
+};
 
 async function init() {
   await movie.fetchGenre(api.getGenreUrl());
@@ -223,30 +185,6 @@ async function init() {
 
   await movie.fetchLatestMovieData(api.getTopRatedUrl(), top_rated_storage);
   // await movie.displayLatestMovieLists(top_rated_storage);
-
-  box_items_container.querySelectorAll(".movie-list__item").forEach(item => {
-    item.addEventListener("mouseenter", fetchCredits);
-  });
-
-  box_items_container.querySelectorAll(".movie-list__item").forEach(item => {
-    item.addEventListener("mouseenter", fetchVideos);
-  });
-
-  box_items_container.querySelectorAll(".movie-list__item").forEach(item => {
-    item.addEventListener("mouseenter", fetchReviews);
-  });
-
-  box_items_container.querySelectorAll(".movie-list__item").forEach(item => {
-    item.addEventListener("click", displayCredits);
-  });
-
-  box_items_container.querySelectorAll(".movie-list__item").forEach(item => {
-    item.addEventListener("click", displayVideos);
-  });
-
-  box_items_container.querySelectorAll(".movie-list__item").forEach(item => {
-    item.addEventListener("click", displayReviews);
-  });
 }
 
 init();
@@ -275,6 +213,15 @@ top_rated_btn.addEventListener('click', (e) => {
   movie.displayLatestMovieLists(top_rated_storage, e.currentTarget.textContent);
 });
 
+list_container.addEventListener('mouseover', (e) => {
+  movie.writeModalContent(e);
+});
+
+list_container.addEventListener('click', (e) => {
+  movie.openModal(e);
+});
+
+modal_close_btn.addEventListener('click', movie.closeModal);
 
 // menu_items.forEach((item) => {
 //   item.addEventListener('click', (e) => {
