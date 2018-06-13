3a7d73b973cb8fe046cea6d8648bbfc831684243
pamcy <chinyi.pam@gmail.com>
2018-06-13 16:50:45 +0800

    [no.45] Add responsive header

--- 45-movie-app/js/main.js
+++ 45-movie-app/js/main.js
@@ -1,10 +1,23 @@
+const container = document.querySelector('.container');
+const hamburger_open_btn = document.querySelector('.header__nav-hambuger-icon');
+const hamburger_close_btn = document.querySelector('.sidebar__close');
 const top_playing_btn = document.querySelector('#js-top-playing');
 const upcoming_btn = document.querySelector('#js-upcoming');
 const top_rated_btn = document.querySelector('#js-top-rated');
 const heading = document.querySelector('.main__heading');
 const list_container = document.querySelector('.main__movie-list');
-const modal = document.querySelector('.overlay');
 const search_field = document.querySelector('#js-search');
+const modal = document.querySelector('.overlay');
+const backdrop = document.querySelector('.overlay__backdrop');
+const vote = document.querySelector('.overlay__votes');
+const poster = document.querySelector('.overlay__poster');
+const name = document.querySelector('.overlay__info-heading');
+const release_date = document.querySelector('.overlay__info-release');
+const runtime = document.querySelector('.overlay__info-runtime');
+const category = document.querySelector('.overlay__info-category');
+const overview = document.querySelector('.overlay__info-overview');
+const casts_container = document.querySelector('.overlay__cast-list');
+const video_container = document.querySelector('.overlay__video-wrapper');
 const genres_storage = {};
 const now_playing_storage = [];
 const upcoming_storage = [];
@@ -13,6 +26,7 @@ const details_storage = [];
 const casts_storage = [];
 const videos_storage = [];
 const search_storage = [];
+let menu_is_open = false;
 let search_timeout = null;
 
 const api = {
@@ -137,6 +151,63 @@ const movie = {
         return videos_storage;
       });
   },
+  fetchSearchData() {
+    const search_dalay_time = 600;
+    const search_value = this.value;
+
+    clearTimeout(search_timeout);
+
+    search_timeout = setTimeout(() => {
+      fetch(api.getSearchUrl(search_value))
+        .then(response => response.json())
+        .then((data) => {
+          const exist = search_storage.findIndex((current) => {
+            return current.value == search_value;
+          });
+
+          if (exist < 0) {
+            search_storage.push({ value: search_value, result: data.results });
+          }
+        })
+        .then(() => document.querySelector('.main').classList.add('is-loading'))
+        .then(() => movie.displaySearchLists(search_value));
+    }, search_dalay_time);
+  },
+  displaySearchLists(keyword) {
+    list_container.innerHTML = '';
+
+    const match_data = search_storage.filter((item) => {
+      return item.value == keyword;
+    });
+
+    const search_content = match_data[0].result.map((item) => {
+      let poster_image = '';
+
+      if (item.poster_path) {
+        poster_image = `<img src="${api.img_prefix}500${item.poster_path}" alt="${item.original_title}" class="main__movie-poster">`;
+      }
+
+      return `
+        <li class="main__movie-item" data-id="${item.id}" data-date=${item.release_date}>
+          <div class="main__movie-top">
+            ${poster_image}
+            <span class="main__movie-votes">${item.vote_average}</span>
+          </div>
+          <div class="main__movie-bottom">
+            <h3 class="main__movie-name">${item.original_title}</h3>
+            <h4 class="main__movie-category">${item.genre_ids.map(id => movie.displayGenres(id)).join(', ')}</h4>
+          </div>
+        </li>
+        `;
+    }).join('');
+
+    const highlight_text = `<span class="main__heading-highlight">${keyword}</span>`;
+
+    heading.innerHTML = `Search results for ${highlight_text}`;
+    list_container.innerHTML = search_content;
+
+    document.querySelector('.main').classList.remove('is-loading');
+  },
   renderModalContent(e) {
     const current_movie = e.target.closest('.main__movie-item');
 
@@ -148,18 +219,7 @@ const movie = {
     const details_data = movie.fetchMovieDetails(movie_id);
     const casts_data = movie.fetchCastsData(movie_id);
     const videos_data = movie.fetchVideosData(movie_id);
-
-    const backdrop = document.querySelector('.overlay__backdrop');
-    const vote = document.querySelector('.overlay__votes');
-    const poster = document.querySelector('.overlay__poster');
-    const name = document.querySelector('.overlay__info-heading');
-    const release_date = document.querySelector('.overlay__info-release');
     const release_date_text = current_movie.dataset.date;
-    const runtime = document.querySelector('.overlay__info-runtime');
-    const category = document.querySelector('.overlay__info-category');
-    const overview = document.querySelector('.overlay__info-overview');
-    const casts_container = document.querySelector('.overlay__cast-list');
-    const video_container = document.querySelector('.overlay__video-wrapper');
 
     details_data.then((data) => {
       const match_data = data.filter(item => item.id == movie_id);
@@ -231,59 +291,11 @@ const movie = {
       }
     });
   },
-  fetchSearchData() {
-    const search_dalay_time = 800;
-    const search_value = this.value;
-
-    clearTimeout(search_timeout);
-
-    search_timeout = setTimeout(() => {
-      return fetch(api.getSearchUrl(search_value))
-        .then(response => response.json())
-        .then((data) => {
-          const exist = search_storage.findIndex((current) => {
-            return current.value == search_value;
-          });
-
-          if (exist < 0) {
-            search_storage.push({ value: search_value, result: data.results });
-          }
-        })
-        .then(() => movie.displaySearchLists(search_value));
-    }, search_dalay_time);
-  },
-  displaySearchLists(keyword) {
-    list_container.innerHTML = '';
-
-    const match_data = search_storage.filter((item) => {
-      return item.value == keyword;
-    });
-
-    const search_content = match_data[0].result.map((item) => {
-      let poster_image = '';
-
-      if (item.poster_path) {
-        poster_image = `<img src="${api.img_prefix}500${item.poster_path}" alt="${item.original_title}" class="main__movie-poster">`;
-      }
-
-      return `
-        <li class="main__movie-item" data-id="${item.id}" data-date=${item.release_date}>
-          <div class="main__movie-top">
-            ${poster_image}
-            <span class="main__movie-votes">${item.vote_average}</span>
-          </div>
-          <div class="main__movie-bottom">
-            <h3 class="main__movie-name">${item.original_title}</h3>
-            <h4 class="main__movie-category">${item.genre_ids.map(id => movie.displayGenres(id)).join(', ')}</h4>
-          </div>
-        </li>
-        `;
-    }).join('');
-
-    const highlight_text = `<span class="main__heading-highlight">${keyword}</span>`;
-
-    heading.innerHTML = `Search results for ${highlight_text}`;
-    list_container.innerHTML = search_content;
+  clearModalContent() {
+    backdrop.style.backgroundImage = '';
+    poster.src = '';
+    vote.textContent = '0';
+    name.textContent = '';
   },
   openModal(e) {
     if (e.target.closest('.main__movie-item')) {
@@ -300,6 +312,15 @@ const movie = {
     }
 
     document.body.removeAttribute('style');
+    movie.clearModalContent();
+  },
+  openMenuList() {
+    container.classList.add('menu-is-open');
+  },
+  closeMenuList(e) {
+    if (e.target == hamburger_close_btn || e.target.closest('.sidebar')) {
+      container.classList.remove('menu-is-open');
+    }
   },
   toggleMenuStyle(e) {
     const current = e.currentTarget;
@@ -308,6 +329,17 @@ const movie = {
       menu.classList.remove('is-active');
     });
     current.classList.add('is-active');
+  },
+  checkMediaQuery() {
+    const breakpoint = window.matchMedia('(max-width: 768px)');
+    const main_section = document.querySelector('.main');
+    const sidebar_height = document.querySelector('.sidebar').offsetHeight;
+
+    if (breakpoint.matches) {
+      main_section.style.marginTop = `${-sidebar_height}px`;
+    } else {
+      main_section.style.marginTop = 0;
+    }
   },
 };
 
@@ -323,6 +355,9 @@ async function init() {
 }
 
 init();
+
+hamburger_open_btn.addEventListener('click', movie.openMenuList);
+hamburger_close_btn.addEventListener('click', movie.closeMenuList);
 
 top_playing_btn.addEventListener('click', (e) => {
   movie.toggleMenuStyle(e);
@@ -355,3 +390,6 @@ modal.addEventListener('click', movie.closeModal);
 search_field.addEventListener('keyup', movie.fetchSearchData);
 search_field.addEventListener('keydown', movie.fetchSearchData);
 search_field.addEventListener('change', movie.fetchSearchData);
+
+// movie.checkMediaQuery();
+// window.addEventListener('resize', movie.checkMediaQuery);
